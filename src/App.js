885901916ebf4ca2dd67bc4a41ebe1b1398310d7
Kaustav Deka <116963260+deka27@users.noreply.github.com>
2023-09-21 21:25:43 +0530

    +footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import d from '../src/assets/d.webp'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-tr from-slate-300 via-gray-400 to-blue-500">
 
-      <div className='flex flex-col gap-4 mx-auto'>
+      <div className='h-2 '></div>
+
+      <div className='flex flex-col gap-2 mx-auto'>
         <div className='side_1 mx-auto'>
           <img src={a} alt='a'></img>
         </div>
@@ -21,7 +23,9 @@ function App() {
         <div className='side_4 mx-auto'>
           <img src={d} alt='d'></img>
         </div>
-
+      </div>
+      <div className='mx-auto bg-black h-16 flex justify-center items-center'>
+        <div className='text-center text-base text-white'>⚡Powered by <span className='font-semibold text-rose-400'>Modubix</span>⚡</div>
       </div>
 
     </div>
